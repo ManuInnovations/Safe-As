b630ee61cf3cd48c219f2b79194fb426c86a71e1
anna-marie-vos <amvos@outlook.com>
2017-05-22 21:57:40 +1200

    first button added

--- safe-as/App.js
+++ safe-as/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from 'react-native-button';
 
 export default class App extends React.Component {
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          containerStyle={styles.button}
+          style={styles.buttonFont}>
+          New Report
+        </Button>
       </View>
     );
   }
@@ -20,4 +28,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding:10,
+    height:45,
+    overflow:'hidden',
+    borderRadius:4,
+    backgroundColor: 'grey'
+  },
+  buttonFont: {
+    fontSize: 20,
+    color: 'white'
+  }
 });
